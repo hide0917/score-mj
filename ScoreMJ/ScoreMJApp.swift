9edf1f198008ca66ca491631515c0797bef1b970
Hidemitsu Shmizu <hide0917>
2022-01-03 13:19:28 +0900

    first commit

---
+++ ScoreMJ/ScoreMJApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreMJApp.swift
+//  ScoreMJ
+//
+//  Created by Hidemitsu Shmizu on 2022/01/03.
+//
+
+import SwiftUI
+
+@main
+struct ScoreMJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
